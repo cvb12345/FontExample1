5fdb637ad1cad1fd76b3ee3afa596a2abdb098f3
cvb12345 <55619928+cvb12345@users.noreply.github.com>
2020-12-15 21:23:18 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/// 인텔리 센스 동작을 위한 코드
+/// <reference path="../lib/p5.global-mode.d.ts" />
+
+let fontD2Coding ;
+let fontD2CodingBold ;
+
+function preload()
+{
+    //fontD2Coding        = loadFont( "./fonts/D2Coding-Ver1.3.2-20180524.ttf" );
+    //fontD2CodingBold    = loadFont( "./fonts/D2CodingBold-Ver1.3.2-20180524.ttf" );
+}
+
+function setup()
+{
+    createCanvas( 640, 320 );
+    console.log("setup()호출됨");
+
+    //textFont( fontD2Coding );
+    textSize( 32 );
+}
+
+function draw()
+{
+    background( 96, 96, 96 );
+
+    textSize( 16 );
+    fill( 255, 255, 0 );
+    text( "ㅋㅋㄹㅃㅃ", mouseX, mouseY );
+
+    textSize( 16 );
+    fill( 0, 255, 255 );
+    text( "ㅋㅋㄹㅃㅃ", mouseX, mouseY + 24 );
+}
+
+
